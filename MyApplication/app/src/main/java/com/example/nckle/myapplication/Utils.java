ce793217c1e464e3b1f679d11565e4cc2129b78b
Unknown <ncklengyel@gmail.com>
2018-05-28 18:21:51 -0400

    Add Utils class

---
+++ MyApplication/app/src/main/java/com/example/nckle/myapplication/Utils.java
@@ -0,0 +1,26 @@
+package com.example.nckle.myapplication;
+
+public class Utils {
+
+    /**
+     * Function that converts a time in milliseconds to a string mm:ss formated
+     * @param ms milliseconds to convert to format mm:ss
+     * @return returns a String in mm:ss format
+     */
+    public static String millisecondToMMSS(int ms){
+
+        int seconds = (ms/1000) % 60;
+        int minutes = (ms/1000) / 60;
+        String time = "0:00";
+
+        //Condition to have 0:00 format and not 0:0 when seconds are <10
+        if (seconds<10){
+            time = Integer.toString(minutes) + ":0" + Integer.toString(seconds);
+        } else {
+            time = Integer.toString(minutes) + ":" + Integer.toString(seconds);
+        }
+
+        return time;
+
+    }
+}
